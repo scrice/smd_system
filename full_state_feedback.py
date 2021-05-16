c2109079632c035a404b450400a819decb5b85e7
scrice <scrice@email.arizona.edu>
2021-05-15 22:58:34 -0700

    raw_smd has actuator model

--- full_state_feedback.py
+++ full_state_feedback.py
@@ -20,27 +20,19 @@ class smd_system:
         self.B = np.array([1/self.m,0])
         self.C = np.array([0,1])
         
-        #sin p state is [p,pdot]
-        # w0 = 0.25 
-        # self.Ac = np.array([[0,1],[-w0**2,0]])
-        # self.Bc = np.array([0,1])
-        # self.Cc = np.array([0,1])
-
-        # double integrator
-        self.K = np.array([-1,-1])
+        self.K = np.array([-2,-1])
         self.F = 4 
-
-        # Arows,Acols = np.shape(self.A) 
-        # Acrows,Accols = np.shape(self.Ac) 
-        # self.Abar = np.block([[self.Ac,np.zeros((Acrows,Acols))], [np.outer(self.B,self.Cc), self.A]])
-        # self.Bbar = np.concatenate([self.Bc,np.zeros(np.size(self.B))])
-        # self.Cbar = np.concatenate([np.zeros(np.size(self.Cc)),self.C])
 
     def plant(self,t,x,u):
         return self.A@x+self.B*u
     
     def closedLoop(self,t,x,reference):
-        return (self.A+np.outer(self.B,self.K))@x+self.B*self.F*reference(t)
+        u = self.K@x+self.F*reference(t)
+        return self.A@x+self.B*u
+
+    def actuatorModel(self,t,x,reference):
+        u = self.K@x+self.F*reference(t)
+        return self.A@x+self.B*u
 
     def simulate(self,x0,t0,tf,reference):
         sol = solve_ivp(self.closedLoop,[t0,tf],x0,args=([reference]), dense_output=True)
@@ -48,8 +40,6 @@ class smd_system:
         z = sol.sol(t)
         y = self.C@z
         sol_plot = plt.plot(t,y)
-        # vel_plot = plt.plot(t,z[3])
-        # pos_plot = plt.plot(t,z[2])
         output = reference(t)
         if np.isscalar(output):
             ref_plot = plt.plot(t,[reference(t)]*len(t))
@@ -64,8 +54,6 @@ class smd_system:
 
 #%%
 mysmd = smd_system(m=1,b=2,k=3)
-# mysmd.simulate(np.array([0,9]),0,50,lambda x:3)
+mysmd.simulate(np.array([0,0]),0,10,lambda x:.25)
 # %%
-mysmd.simulate(np.array([0,2]),0,50,lambda x:0.1*np.sin(0.25*x))
-
-# print(linalg.eig(mysmd.Abar))
+# mysmd.simulate(np.array([0,2]),0,50,lambda x:0.1*np.sin(0.25*x))
--- raw_smd.py
+++ raw_smd.py
@@ -9,27 +9,39 @@ A = np.array([[0,1],[-k/m,-b/m]])
 B = np.array([0,1/m])
 C = np.array([1,0])
 
-T = 50
-dt = 0.1
+T = 20
+dt = 0.001
 
-track = 2
+track = 3
 if track == 1:
     kp=1
     kd=2
     ki=3
+    actuator_model = 0
     r = lambda t:3
 
 if track == 2:
-    kp=3
+    kp=1
     kd=2
     ki=15
+    actuator_model = 0
     r = lambda t:0.1*np.sin(.25*t)
 
-x = np.array([2,0])
+if track == 3:
+    kp = 3
+    kd = 2
+    ki = .25 
+    r = lambda t:.25
+    actuator_model = 1
+
+x = np.array([0,0])
+off_start_time = -1
 
 all_x = x
 t_prev =-100;e_prev=0;I=0
 t_span = np.arange(0,T,dt)
+u_prev = 0
+u_stack = []
 for t in t_span.tolist():
     e = r(t)-x[0]
 
@@ -40,18 +52,49 @@ for t in t_span.tolist():
         u = (P+I+D)
     else:
         u=P
+    
+    if(actuator_model):
+        if(u>0):
+            if u_prev == 0 and t-off_start_time<0.05:
+                u=0
+            else:
+                u=1
+                if u_prev == 0:
+                    fire_start_time = t
+        if(u<=0):
+            if u_prev == 1 and t-fire_start_time<0.05:
+                u = 1
+            else:
+                u = 0
+                if u_prev == 1:
+                    off_start_time = t
 
     e_prev = e
     t_prev = t
+    u_prev = u
 
     x = x+dt*(A@x+B*u)
     all_x=np.vstack((all_x,x))
+    u_stack.append(u)
 
-plt.plot(t_span,all_x[:][:-1])
+# plt.plot(t_span,all_x[:][:-1])
+# output = r(t_span)
+# if np.isscalar(output):
+#     ref_plot = plt.plot(t_span,[r(t_span)]*len(t_span))
+# else:
+#     ref_plot = plt.plot(t_span,r(t_span))
+# # plt.plot(t_span,u_stack)
+# plt.legend(["x(t)","x'(t)","r(t)"])
+# plt.show()
+
+fig, axs = plt.subplots(2)
+fig.suptitle('Two Responses')
+axs[0].plot(t_span,all_x[:][:-1])
 output = r(t_span)
 if np.isscalar(output):
-    ref_plot = plt.plot(t_span,[r(t_span)]*len(t_span))
+    ref_plot = axs[0].plot(t_span,[r(t_span)]*len(t_span))
 else:
-    ref_plot = plt.plot(t_span,r(t_span))
-plt.legend(["x(t)","x'(t)","r(t)"])
+    ref_plot = axs[0].plot(t_span,r(t_span))
+axs[0].legend(["x(t)","x'(t)","r(t)"])
+axs[1].plot(t_span,u_stack)
 plt.show()
